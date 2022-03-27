91cb03e30673a22229f15b3c7af9d5ca5fe498ea
Austine Mwanyalo <mwanyaloa@outlook.com>
2022-03-28 02:19:55 +0300

    added nav links to navbar

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
-// import { User, UserService } from '../../core';
+import { AuthService } from 'src/app/core/services/auth-service.service';
 
 @Component({
   selector: 'app-layout-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-  constructor(
-    // private userService: UserService
-  ) {}
-
-  // currentUser: User;
+  currentUser: any = '';
+  appShowAuthed: boolean = false;
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-  //   this.userService.currentUser.subscribe(
-  //     (userData) => {
-  //       this.currentUser = userData;
-  //     }
-  //   );
+    this.isLoggedIn();
   }
+
+  isLoggedIn = () => {
+    this.currentUser = this.authService.getToken();
+
+    if (this.currentUser !== null) {
+      this.appShowAuthed = true;
+    } else {
+      this.appShowAuthed = false;
+    }
+  };
 }
